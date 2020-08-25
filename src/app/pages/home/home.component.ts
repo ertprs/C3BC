d1968309c3d380690d601876d1b66d47d7990aed
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-08-25 01:36:32 -0300

    Refatoração: nova organização de login e home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { MatTabGroup, MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private _addButtonMessage = new BehaviorSubject<string>("Adicionar resposta")
+  @ViewChild("tabGroup") tabGroup: MatTabGroup;
 
-  constructor(
-  ) {}
+  constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.tabGroup.selectedTabChange.subscribe( (selectedTabChange: MatTabChangeEvent) => this.buttonMessage = selectedTabChange.tab.textLabel)
+  }
+
+  get buttonMessage(): string {
+    return this._addButtonMessage.value
+  }
+
+  set buttonMessage(tabName: string) {
+    const newTabName = tabName.slice(0, -1).toLowerCase()
+
+    this._addButtonMessage.next(`Adicionar ${newTabName}`)
+  }
 }
