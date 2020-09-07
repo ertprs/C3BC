3b420d67a6d25616922f0c11fdedf05341480cef
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-09-07 10:23:34 -0300

    Refatoração no modo de adicionar diálogo na página

--- src/inject/injectUdemy.js
+++ src/inject/injectUdemy.js
@@ -6,43 +6,7 @@ function checkIfTheC3BCDialogIsOpen() {
 	return document.querySelector("dialog[id='C3BC-dialog'][open]");
 }
 
-function addC3BCButton() {
-		const cod3rButton = document.createElement("button");
-		cod3rButton.innerHTML = "Cod3r";
-		cod3rButton.classList.add("btn");
-		cod3rButton.setAttribute("type", "button");
-		cod3rButton.setAttribute("id", "cod3r-button");
-		cod3rButton.onclick = clickEvent => {
-			clickEvent.preventDefault();
-			showC3CBDialog();
-		}
-
-		// aqui está sendo capturado um botão e depois pegando o seu pai porque, dentro da div abaixo cuja propriedade data-purpose é igual a "menu-bar",
-		// há duas divs que têm como classe btn-group. A que apresenta algum botão dentro é o nosso alvo. 
-		const formButtonsGroup = document.querySelector("div[data-purpose='menu-bar'] div.btn-group button").parentNode;
-		formButtonsGroup.insertAdjacentElement("beforeend", cod3rButton);
-
-		addC3CBDialog();
-}
-
-function positionDialog() {
-	const C3CBDDialogElementInDOM = document.getElementById("C3BC-dialog");
-	const replyFormSelector = "#br > div.main-content-wrapper > div.main-content > div > div > div.main_container > div > div > div.question-answer--question-answer-content--s7QRB.question-answer--two-pane-mode--1Biaw > div > div.two-pane--container__right-pane--2xMVx > div > div.reply-form--reply-form--GZtNK";
-	
-	const replyFormRect = document.querySelector(replyFormSelector).getBoundingClientRect();
-	const cod3rButtonRect = document.getElementById("cod3r-button").getBoundingClientRect();
-
-	C3CBDDialogElementInDOM.style.top = `${replyFormRect.top-600}px`;
-
-	if( (cod3rButtonRect.right + 360) < document.body.getBoundingClientRect().width)
-		C3CBDDialogElementInDOM.style.left = `${cod3rButtonRect.right}px`;
-	else if( (cod3rButtonRect.right - cod3rButtonRect.width/2 +  180) < document.body.getBoundingClientRect().width || (cod3rButtonRect.left - 360) < 0 )
-		C3CBDDialogElementInDOM.style.left = `${cod3rButtonRect.right - cod3rButtonRect.width/2 - 180}px`;
-	else
-		C3CBDDialogElementInDOM.style.left = `${cod3rButtonRect.left - 360}px`;
-}
-
-function addC3CBDialog() {
+(function addC3CBDialog() {
 	const C3CBDialogElement = document.createElement("dialog");
 	C3CBDialogElement.id = "C3BC-dialog";
 	C3CBDialogElement.setAttribute(
@@ -62,25 +26,31 @@ function addC3CBDialog() {
 
 	C3CBDialogElement.innerHTML = `<iframe src=${chrome.extension.getURL("index.html")} style="height:100%; width:100%;" frameBorder="0"></iframe>`;
 	C3CBDialogElement.addEventListener("click", dialogClickOutsideHandler);
-
-	document.body.appendChild(C3CBDialogElement);
 	
-	positionDialog();
-	window.addEventListener("resize", positionDialog);
+	document.body.appendChild(C3CBDialogElement);
+})();
+
+function positionDialog() {
+	const C3CBDDialogElementInDOM = document.getElementById("C3BC-dialog");
+	const replyFormSelector = "#br > div.main-content-wrapper > div.main-content > div > div > div.main_container > div > div > div.question-answer--question-answer-content--s7QRB.question-answer--two-pane-mode--1Biaw > div > div.two-pane--container__right-pane--2xMVx > div > div.reply-form--reply-form--GZtNK";
+	
+	const replyFormRect = document.querySelector(replyFormSelector).getBoundingClientRect();
+	const cod3rButtonRect = document.getElementById("cod3r-button").getBoundingClientRect();
+
+	C3CBDDialogElementInDOM.style.top = `${replyFormRect.top-600}px`;
+
+	if( (cod3rButtonRect.right + 360) < document.body.getBoundingClientRect().width)
+		C3CBDDialogElementInDOM.style.left = `${cod3rButtonRect.right}px`;
+	else if( (cod3rButtonRect.right - cod3rButtonRect.width/2 +  180) < document.body.getBoundingClientRect().width || (cod3rButtonRect.left - 360) < 0 )
+		C3CBDDialogElementInDOM.style.left = `${cod3rButtonRect.right - cod3rButtonRect.width/2 - 180}px`;
+	else
+		C3CBDDialogElementInDOM.style.left = `${cod3rButtonRect.left - 360}px`;
 }
-
-const addC3CBButtonAndDialogCheckInterval = setInterval( () => {
-	if (document.readyState === "complete" && checkIfTheFormWasLoaded()) {
-		addC3BCButton();
-		addC3CBDialog();
-
-		clearInterval(addC3CBButtonAndDialogCheckInterval);
-	}
-}, 20);
 
 function showC3CBDialog() {
 	const C3CBDDialogElementInDOM = document.getElementById("C3BC-dialog");
 	positionDialog();
+	window.addEventListener("resize", positionDialog);
 	C3CBDDialogElementInDOM.showModal();
 
 	const C3BCDialogIsOpenCheckInterval = setInterval( () => {
@@ -90,6 +60,31 @@ function showC3CBDialog() {
 		}
 	}, 10)
 }
+
+function addC3BCButton() {
+		const cod3rButton = document.createElement("button");
+		cod3rButton.innerHTML = "Cod3r";
+		cod3rButton.classList.add("btn");
+		cod3rButton.setAttribute("type", "button");
+		cod3rButton.setAttribute("id", "cod3r-button");
+		cod3rButton.onclick = clickEvent => {
+			clickEvent.preventDefault();
+			showC3CBDialog();
+		}
+
+		// aqui está sendo capturado um botão e depois pegando o seu pai porque, dentro da div abaixo cuja propriedade data-purpose é igual a "menu-bar",
+		// há duas divs que têm como classe btn-group. A que apresenta algum botão dentro é o nosso alvo. 
+		const formButtonsGroup = document.querySelector("div[data-purpose='menu-bar'] div.btn-group button").parentNode;
+		formButtonsGroup.insertAdjacentElement("beforeend", cod3rButton);
+	}
+	
+const addC3CBButtonAndDialogCheckInterval = setInterval( () => {
+	if (document.readyState === "complete" && checkIfTheFormWasLoaded()) {
+		addC3BCButton();
+
+		clearInterval(addC3CBButtonAndDialogCheckInterval);
+	}
+}, 20);
 
 function setCSSToReplyFormOpen() {
 	const replyFormSelector = "#br > div.main-content-wrapper > div.main-content > div > div > div.main_container > div > div > div.question-answer--question-answer-content--s7QRB.question-answer--two-pane-mode--1Biaw > div > div.two-pane--container__right-pane--2xMVx > div > div.reply-form--reply-form--GZtNK";
