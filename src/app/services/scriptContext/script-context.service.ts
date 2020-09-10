47e6b36327ef4685f2e14920a6af7874fe087333
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-09-10 16:46:12 -0300

    Botão de adicionar acrescentado às respostas exibidas a partir das categorias

--- src/app/services/scriptContext/script-context.service.ts
+++ src/app/services/scriptContext/script-context.service.ts
@@ -21,4 +21,12 @@ export class ScriptContextService {
       };
     });
   }
+
+  insertAnswer(answerContent: string) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {answerContent}, function(response) {
+        console.log(response.farewell);
+      });
+    });
+  }
 }
